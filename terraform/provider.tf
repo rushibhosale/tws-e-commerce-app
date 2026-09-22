823179e4d9b4c7c09ea3f8a3cdd3316c82a82b12
Rushikesh <rushikeshbhosale2014@gmail.com>
2026-09-22 15:53:34 +0000

    updated azs

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ locals {
   region          = "us-east-1"
   name            = "tws-eks-cluster"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["eu-west-1a", "eu-west-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
